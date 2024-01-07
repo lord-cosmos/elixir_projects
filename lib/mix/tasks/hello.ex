c422eb0d1923e29281bc7f07aaba5dd789d3895b
lord-cosmos <teslaatozinspires7@gmail.com>
2024-01-07 10:57:42 -0500

    add mix hello : new custom mix task

---
+++ lib/mix/tasks/hello.ex
@@ -0,0 +1,13 @@
+defmodule Mix.Tasks.Hello do
+  @moduledoc "The hello mix task: `mix help help`"
+  use Mix.Task
+
+  @shortdoc "Simply calls the Hello.say/0 function."
+  def run(_) do
+    # This will start our application
+    Mix.Task.run("app.start")
+
+    # calling our Hello.say() function frome earlier
+    Hello.say()
+  end
+end
