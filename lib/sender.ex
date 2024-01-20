a74a18f49e6c2158fcdc6b64de378e3c4cf2df44
lord-cosmos <teslaatozinspires7@gmail.com>
2024-01-19 22:41:50 -0500

    add async email notifications

---
+++ lib/sender.ex
@@ -0,0 +1,17 @@
+defmodule Sender do
+  def send_email(email) do
+    Process.sleep(3000)
+    IO.puts "Email to #{email} sent"
+    {:ok, "email_sent"}
+  end
+
+
+  def notify_all(emails) do
+    Enum.each(emails, fn email ->
+      Task.start(fn -> send_email(email)
+      end)
+    end)
+  end
+
+  
+end
