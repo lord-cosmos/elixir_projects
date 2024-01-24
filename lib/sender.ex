01ea87f90dc0d036c1754a8e1fe988d31ebd22c9
lord-cosmos <teslaatozinspires7@gmail.com>
2024-01-23 22:17:23 -0500

    chore: fix notes

--- lib/sender.ex
+++ lib/sender.ex
@@ -1,18 +1,3 @@
-defmodule Sender do
-  def send_email(email) do
-    Process.sleep(3000)
-    IO.puts "Email to #{email} sent"
-    {:ok, "email_sent"}
-  end
-
-
-  def notify_all(emails) do
-   emails
-   |> Task.async_stream(&send_email/1, max_concurrency: 1000, ordered: false)
-   |> Enum.to_list()
-  end
-
-end
 
 # Task.start or Task.async will start the process asynchronously
 # To yield the result of the process start, use Task.await or Task.yield
@@ -34,3 +19,20 @@ end
 # The shutdown/1 function also accepts a timeout and gives the process a last chance to com- plete, before stopping it.
 # If it completes, you will receive the result as normal.
 # You can also stop a process immediately (and rather violently) by using the atom :brutal_kill as a second argument.
+
+
+defmodule Sender do
+  def send_email(email) do
+    Process.sleep(3000)
+    IO.puts "Email to #{email} sent"
+    {:ok, "email_sent"}
+  end
+
+
+  def notify_all(emails) do
+   emails
+   |> Task.async_stream(&send_email/1, max_concurrency: 1000, ordered: false)
+   |> Enum.to_list()
+  end
+
+end
