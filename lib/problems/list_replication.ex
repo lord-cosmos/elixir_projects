d37125244c6b947af70c85489fade31985932f49
lord-cosmos <teslaatozinspires7@gmail.com>
2024-02-19 20:49:30 -0500

    add problems section - put list replication

---
+++ lib/problems/list_replication.ex
@@ -0,0 +1,14 @@
+defmodule Problems.ListReplication do
+  def elements([head|tail]) do
+      Enum.each(tail, fn x ->
+          Enum.each(1..head, fn _ -> IO.puts(x) end)
+      end)
+  end
+  def main() do
+      IO.read(:stdio, :all)
+          |> String.split
+          |> Enum.map(&String.to_integer(&1))
+          |> elements
+  end
+end
+Problems.ListReplication.main
