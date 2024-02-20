00a33b4a2822a31b5207c127b2fea535d03420fd
lord-cosmos <teslaatozinspires7@gmail.com>
2024-02-20 11:04:21 -0500

    add array_n_elements - learn list comprehension

---
+++ lib/problems/array_n_elements.ex
@@ -0,0 +1,22 @@
+# Link  : https://www.hackerrank.com/challenges/fp-array-of-n-elements/
+
+defmodule Problems.ArrayNElements do
+  def takeInteger() do
+      IO.gets("") |> String.trim() |> String.to_integer()
+  end
+
+  def takeInput() do
+      n = takeInteger()
+      printList(n)
+  end
+
+  def printList(n) do
+      # create an array [1,1, ... n times]and print it in same way
+      result = for _ <- 1..n, do: 1
+      IO.puts Enum.join(result, ", ")
+  end
+
+end
+
+
+Problems.ArrayNElements.takeInput()
