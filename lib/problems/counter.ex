1bbe44baa9f577c371a187bb56243d8da919f376
lord-cosmos <teslaatozinspires7@gmail.com>
2024-04-16 14:12:19 -0400

    add counter program - struct

---
+++ lib/problems/counter.ex
@@ -0,0 +1,29 @@
+defmodule Counter do
+  defstruct count: 0
+ def incrementBy(counter, by) when is_integer(by) do
+    %{counter | count: counter.count + by}
+  end
+
+  def incrementBy(counter, by) when is_float(by) do
+    %{counter | count: counter.count + (by)}
+  end
+
+
+  def decrementBy(counter, by) when is_integer(by) do
+    %{counter | count: counter.count - by }
+  end
+
+end
+
+defmodule Main do
+  def run() do
+    counter = %Counter{}
+    counter = Counter.incrementBy(counter, 5.00)
+    IO.puts(counter.count)
+
+    counter2 = Counter.decrementBy(counter, 3)
+    IO.puts counter2.count
+  end
+end
+
+Main.run()
