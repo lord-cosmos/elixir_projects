2a6c2ee24d88f9f6a0b5bd456c727dfb385c1b84
lord-cosmos <teslaatozinspires7@gmail.com>
2024-05-04 20:07:01 -0400

    add map() impl

--- lib/problems/map_function.ex
+++ lib/problems/map_function.ex
@@ -4,5 +4,16 @@ defmodule MyList.Utils do
     [func.(head) | map(tail, func)]
   end
 
-  
+  def reduce([], value, _func), do: value
+  def reduce([head | tail], value, func) do
+    reduce(tail,  func.(head, value), func)
+  end
+
+end
+
+
+defmodule Swapper do
+  def swap([]), do: []
+  def swap([a,b | tail]), do: [b,a | swap(tail)]
+  def swap([_]),do: raise "single element, not possible to swap."
 end
